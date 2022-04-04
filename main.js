1749c219c8229496e8cc547dd92d32a323361488
Yulianny Betancourt <flipsdulce@gmail.com>
2022-04-04 13:29:28 -0500

    :wheelchair: Improve accessibility to 100%

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ import "./style.css";
 
 /* Menú mobile */
 const toggleButton = document.querySelector(".hamburguer");
-
 const menuHamburguer = document.querySelector(".menu");
-
 toggleButton.addEventListener("click", () => {
   menuHamburguer.classList.toggle("active-hamburger");
 });
